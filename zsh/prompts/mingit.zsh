39b02a6e69dd2eb72a8f6776b1eb8a88f0f9b4ff
Ian Liao <ianryanliao@gmail.com>
2016-08-01 00:46:09 -0700

    workingon auto starting tmux
    Former-commit-id: 65772ee3a4f5b4ec86c6cb1c4c98f66b7aedceb1

--- zsh/prompts/mingit.zsh
+++ zsh/prompts/mingit.zsh
@@ -1,6 +1,5 @@
 #
 # mingit theme
-# ❯ ➜
 #
 
 # # Unused functions
@@ -31,7 +30,7 @@ gst_get_pwd() {
   local prompt_short_dir="$(short_pwd)"
   local git_root="$(command git rev-parse --show-toplevel 2> /dev/null)"
   local prompt_short_dir="${prompt_short_dir#${$(short_pwd $git_root):h}/}"
-  print "%F{8}${prompt_short_dir}%f"
+   print "%F{8}${prompt_short_dir}%f"
 }
 
 gst_get_rprompt() {
@@ -39,11 +38,6 @@ gst_get_rprompt() {
 }
 
 prompt_mingit_precmd() {
-  # hzle -N zle-line-init
-  #) zle -N zle-keymap-select
-  # zle -N zle-line-finish
-
-  PROMPT="$(gst_get_prefix)$(gst_get_pwd)$(parse_git_dirty)$(git_prompt_info)$(gst_get_suffix)"
 }
 
 # function zle-line-init zle-line-finish zle-keymap-select {
@@ -52,16 +46,20 @@ prompt_mingit_precmd() {
 # }
 
 prompt_mingit_setup() {
+  setopt LOCAL_OPTIONS
+  unsetopt XTRACE KSH_ARRAYS
+  prompt_opts=(cr percent subst)
+
+  autoload -Uz add-zsh-hook
+
   ZSH_THEME_GIT_PROMPT_PREFIX=""
   ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
   ZSH_THEME_GIT_PROMPT_DIRTY=" %F{1}"
   ZSH_THEME_GIT_PROMPT_CLEAN=" %F{6}"
-  # ZSH_THEME_GIT_PROMPT_UNTRACKED=" %F{3}"
-  #
-  autoload -Uz add-zsh-hook
+
   add-zsh-hook precmd prompt_mingit_precmd
 
-  prompt_opts=(cr percent subst)
+  PROMPT="$(gst_get_prefix)$(gst_get_pwd)$(parse_git_dirty)$(git_prompt_info)$(gst_get_suffix)"
 }
 
 prompt_mingit_setup "$@"
