7b9a6bb0279e11f9350ff6dc7916b87aa705829f
Ian Liao <ianryanliao@gmail.com>
2016-08-27 01:18:57 -0700

    wrapping up lingering changes
    Former-commit-id: 078c2a6a8fe0a91beac1efd5336acb8518f5a847

--- zsh/prompts/mingit.zsh
+++ zsh/prompts/mingit.zsh
@@ -2,22 +2,6 @@
 # mingit theme
 #
 
-# # Unused functions
-# # require ~/.bin/battery_info.py included in PATH
-# function battery_charge() {
-#   # echo `$BAT_CHARGE` 2>/dev/null
-#   if [[ -e ~/bin/batcharge.py ]] then
-#     echo `python ~/bin/batcharge.py`;
-#   else
-#     echo '';
-#   fi
-# }
-
-# git_custom_prompt_info() {
-#   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-#   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-# }
-
 gst_get_prefix() {
   print "%(?:%F{6} :%F{6} %s)%f"
 }
@@ -33,18 +17,10 @@ gst_get_pwd() {
   print ${prompt_short_dir}
 }
 
-# gst_get_rprompt() {
-#   print "%F{15}%*%f"
-# }
-
 prompt_mingit_precmd() {
   PROMPT="$(gst_get_prefix)%F{8}$(gst_get_pwd)%f$(parse_git_dirty)$(git_prompt_info)$(gst_get_suffix)"
+  RPROMPT=""
 }
-
-# function zle-line-init zle-line-finish zle-keymap-select {
-#   zle reset-prompt
-#   zle -R
-# }
 
 prompt_mingit_setup() {
   ZSH_THEME_GIT_PROMPT_PREFIX=""
@@ -52,20 +28,7 @@ prompt_mingit_setup() {
   ZSH_THEME_GIT_PROMPT_DIRTY=" %F{1}"
   ZSH_THEME_GIT_PROMPT_CLEAN=" %F{6}"
 
-
   autoload -Uz add-zsh-hook
-
-  # # TODO: verify this fixes window size with Tmux
-  # # Unsets the Terminal.app current working directory when a terminal
-  # # multiplexer or remote connection is started since it can no longer be
-  # # updated, and it becomes confusing when the directory displayed in the title
-  # # bar is no longer synchronized with real current working directory.
-  # function _terminal-unset-terminal-app-proxy-icon {
-  #   if [[ "${2[(w)1]:t}" == (screen|tmux|dvtm|ssh|mosh) ]]; then
-  #     print '\e]7;\a'
-  #   fi
-  # }
-  # add-zsh-hook preexec _terminal-unset-terminal-app-proxy-icon
 
   add-zsh-hook precmd prompt_mingit_precmd
   prompt_opts=(cr subst percent)
--- zsh/ztmux.zsh
+++ zsh/ztmux.zsh
@@ -7,17 +7,19 @@ fi
 # if [[ -z "$TMUX" && -z "EMACS" && -z "$VIM" && ( [[ -n "$SSH_TTY"]] || [[ -z "$SSH_TTY" ]] ) ]]; then # TODO: check if -z $EMACS && -z $VIM is needed
 if [[ -z "$TMUX" ]]; then
   tmux start-server
-
+  tmux_session='zmux'
   # Create a session named 'zmux' if no session has been defined in tmux.conf
   if ! tmux has-session 2> /dev/null; then
-    tmux_session='zmux'
+      set -- $(stty size) # $1 = rows $2 = columns
       tmux \
-        new-session -d -s "$tmux_session" \; \
-        set-option -t "$tmux_session" destroy-unattached off &> /dev/null
+        new-session -d -s "$tmux_session" -x "$2" -y "$(($1 - 1))" \; \
+        attach-session -t "$tmux_session" \; \
+        set destroy-unattached
   fi
 
+  # TODO: verify this works properly
   # attach to the session named 'zmux' or to the last session used.
-  exec tmux attach-session
+  exec tmux
 fi
 
 # TODO: check if this implementation works, may need to put at end of /root/.zshrc
