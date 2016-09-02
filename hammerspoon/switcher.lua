302ed34b616f883f0d3f02d5f2d618bf91ae6245
Ian Liao <ianryanliao@gmail.com>
2016-09-01 23:58:56 -0700

    window and switcher enhancement in hammerspoon
    Former-commit-id: 7bd8d0415da7d460b102082242b3bc47078e0d1b

--- hammerspoon/switcher.lua
+++ hammerspoon/switcher.lua
@@ -5,6 +5,7 @@ wf_all = hs.window.filter.new() -- only visible windows, all Spaces
 uiPrefsAll = {
   fontName = 'Hack',
   textSize = 12,
+  highlightColor = {0,0,0,0},
   backgroundColor = {0,0,0,0},
   onlyActiveApplication = false,
   showTitles = false,
@@ -20,15 +21,16 @@ wf_active_app = hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{} 
 uiPrefsActiveApp = {
   fontName = 'Hack',
   textSize = 12,
-  backgroundColor = {1,0,0,0},
+  highlightColor= {1,0.9,0,0.8},
+  backgroundColor = {0.3,0.3,0.3,1},
   onlyActiveApplication = true,
   showTitles = true,
   showThumbNails = false, -- screen as icon
-  thumbnailSize = 175,
+  thumbnailSize = 150,
   showSelectedThumbnail = false, -- screen as popup background
   showSelectedTitle = false
 
 }
-switcher_active_app = hs.window.switcher.new(wf_active_app, uiPrefsActiveApp, nil, nil) -- include minimized/hidden windows, current Space only
+switcher_active_app = hs.window.switcher.new(wf_active_app, uiPrefsActiveApp) -- include minimized/hidden windows, current Space only
 
 -- switcher = hs.window.switcher.new{'Safari','Google Chrome'} -- specialized switcher for your dozens of browser windows :)
