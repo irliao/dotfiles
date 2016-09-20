c8376d0fc61981413556eb2c2cb7c87e4e646b70
Ryan Liao <ryan_liao@intuit.com>
2016-09-20 15:41:25 -0700

    removing Hammerspoon highlights
    Former-commit-id: 831156047dab9c62c8c423a40f198551baa43aaf

--- hammerspoon/highlight.lua
+++ hammerspoon/highlight.lua
@@ -19,8 +19,8 @@ hs.window.highlight.ui.frameWidth = 5
 hs.window.highlight.ui.frameColor = {1,0.9,0,0.5} -- yellow
 hs.window.highlight.ui.flashDuration= 0
 
--- TODO: handle existing corner cases
-wf_highlight:subscribe(hs.window.filter.windowFocused, toggleWindowHighlight)
-wf_highlight:subscribe(hs.window.filter.windowUnfocused, function() hs.window.highlight.stop() end) -- TODO: test this
+-- TODO: handle existing corner cases, ex: overlay for unfocused windows is short on height
+-- wf_highlight:subscribe(hs.window.filter.windowFocused, toggleWindowHighlight)
+-- wf_highlight:subscribe(hs.window.filter.windowUnfocused, function() hs.window.highlight.stop() end) -- TODO: test this
 
 
