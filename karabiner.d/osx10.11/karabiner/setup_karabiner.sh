4b813cd300ebf6cac33d18462fb25a051b62ace6
Ryan Liao <ryan_liao@intuit.com>
2016-10-03 09:30:06 -0700

    fix karabiner symlink for osx10.11
    Former-commit-id: d0dedc4d5e3bcc7b56653697c95233de408e966c

--- karabiner.d/osx10.11/karabiner/setup_karabiner.sh
+++ karabiner.d/osx10.11/karabiner/setup_karabiner.sh
@@ -2,10 +2,10 @@
 
 # TODO: refactor to check if file exists before removing private.xml
 mv ~/Library/Application\ Support/Karabiner/private.xml ~/Downloads/private-bak.xml &&
-    ln -s ~/.dotfiles/pqrs/private.xml ~/Library/Application\ Support/Karabiner/private.xml;
+    ln -s ~/.dotfiles/karabiner.d/osx10.11/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml;
 
 
-fPath=${HOME}/.dotfiles/pqrs/karabiner/import_karabiner_settings.sh
+fPath=${HOME}/.dotfiles/karabiner.d/osx10.11/karabiner/import_karabiner_settings.sh
 if [[ -f $fPath ]]; then
 	source $fPath
 fi
