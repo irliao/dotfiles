649bf6093b1e31034c93accc2e2a8fd341242e96
Ryan Liao <ianryanliao@gmail.com>
2021-04-04 18:53:59 -0700

    added fzf scripts for command history search

---
+++ config/fzf/fzf.bash
@@ -0,0 +1,16 @@
+# Setup script from fzf for Bash (created from fzf install)
+# Source to enable ^R binding to search history with fzf
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
